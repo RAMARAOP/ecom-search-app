9848fa2938cb2db6f96c769ed88e6a4f98900487
Rama <ramapillem@gmail.com>
2025-08-05 15:30:13 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//cors middleware
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
